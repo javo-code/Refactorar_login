3e87044136fbd5ebec023e5a4f8ec0c7587c5a6d
Romero Javier <romerofj26@gmail.com>
2024-01-03 21:45:24 -0300

    secure token updated

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -5,13 +5,10 @@ const userDao = new UserDao();
 
 export const checkToken = async (req, res, next) => {
   try {
-    // const authHeader = req.headers('Authorization') - otra psoibilidad
-    const authHeader = req.get("Authorization");
-    // 'Bearer gdfgdfgdg16dfg65df4g564dfg¿??=¿?=¿?' <= asi recibimos el token , Desp con un split() quitaremos esa prefijo "Bearer"
-    if (!authHeader) return res.status(401).json({ msg: "Unauthorized" });
-    const token = authHeader.split(" ")[1];
-    // esto es lo que obetenemos desp del split() = 'gdfgdfgdg16dfg65df4g564dfg¿??=¿?=¿?'
-    const decode = jwt.verify(token, PRIVATE_KEY);
+
+    const cookieToken = req.cookies.token
+    if (!cookieToken) return res.status(401).json({ msg: "Unauthorized" });
+    const decode = jwt.verify(cookieToken, PRIVATE_KEY);
     console.log("👹Token Decodificado => ", decode); //payload ---> {userId: id de mongo},este es el objeto que viene desde la fn "generateToken" en auth.js 
     const user = await userDao.getById(decode.userId);
     if (!user) return res.status(401).json({ msg: "Unauthorized" });
